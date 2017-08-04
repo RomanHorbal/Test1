6f56c875ce0d1c319d382b2492c3dc57be9ed732
User <User@WIN-TGGV4P2R0MT>
2017-08-04 18:52:23 +0300

    1

--- Test1/src/ua/main.java
+++ Test1/src/ua/main.java
@@ -3,7 +3,7 @@ package ua;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("I am hero!");
+		System.out.println("Hallo");
 		System.out.println("I am hero!");
 	}
 
